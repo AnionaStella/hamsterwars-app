560135dbea7fa57a1ae95e00cfa9cb5a6a6e25c7
AnionaStella <eiljon88@gmail.com>
2020-06-15 17:27:37 +0200

    trying to access routes in heroku

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -66,7 +66,7 @@ const Battle = () => {
             </div>
             <button onClick={() => createNewGame(setToggleNewGame, toggleNewGame, setWinner)}>New battle</button>
             <br/>
-            <Link to={`/matchup/${hamster1.id}/${hamster2.id}`}> See results </Link>
+            <Link to={ hamster1 && hamster2 ? `/matchup/${hamster1.id}/${hamster2.id}` : ''}> See results </Link>
         </div>
     )
 }

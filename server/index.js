01ebef28e5a4ee2493ed7e35cea358bc5d245bda
AnionaStella <eiljon88@gmail.com>
2020-06-15 16:54:54 +0200

    trying to access routes in heroku

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ const cors = require('cors')
 app.use(cors())
 app.options('*', cors())
 app.use(express.json())
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '/../build')));// Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '/../build', 'index.html'));
+    });
+  }
 
 app.use(express.static(__dirname + '/../build'));
 
